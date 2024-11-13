8e6c36444dab2a9834cd32c43e469cdd44c8321b
Delfin F <DelfinoDevShark@gmail.com>
2024-11-13 20:54:06 +0100

    aria-label for accesibility

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,6 +15,7 @@ const SearchBar = ({searchQuery, setSearchQuery, region, setRegion}) => {
 				/>
 			</div>
 			<select
+				aria-label="Filter by Region"
 				value={region}
 				onChange={e => setRegion(e.target.value)}
 				className="px-4 py-2 border rounded shadow-sm w-full md:w-auto dark:bg-dark-blue dark:text-white dark:border-0"
