1598b942092005fcf1d7b86544a9b3a40fb6d352
Delfin F <DelfinoDevShark@gmail.com>
2024-11-13 18:52:20 +0100

    navbar component: styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 // src/components/Navbar.js
-import {useState} from "react";
+//import {useState} from "react";
 
-const Navbar = ({toggleDarkMode}) => {
+const Navbar = ({toggleDarkMode, mode}) => {
 	return (
-		<header className="bg-white dark:bg-dark-blue shadow-md py-4 px-8 flex justify-between items-center">
-			<h1 className="text-xl font-bold">Where in the world?</h1>
+		<header className="bg-white dark:text-white dark:bg-dark-blue shadow-md py-4 px-8 flex justify-between items-center">
+			<h1 className="text-2xl font-bold py-3">Where in the world?</h1>
 			<button onClick={toggleDarkMode} className="text-sm">
-				Toggle Dark Mode
+				<span>{mode ? " ☀️ " : " 🌙 "}</span>
+				<span className="text-lg">Dark Mode</span>
 			</button>
 		</header>
 	);
