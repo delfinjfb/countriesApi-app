12eb83c929b6ec76814d5d6735153b2b70701710
Delfin F <DelfinoDevShark@gmail.com>
2024-11-23 01:07:58 +0100

    matomo: disable cookies

--- src/utils/Matomo.js
+++ src/utils/Matomo.js
@@ -1,24 +1,25 @@
-import { useEffect } from 'react';
+import {useEffect} from "react";
 
 const Matomo = () => {
-  useEffect(() => {
-    const _paq = (window._paq = window._paq || []);
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    
-    const u = 'https://delfinowebsite.matomo.cloud/';
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '2']);
+	useEffect(() => {
+		const _paq = (window._paq = window._paq || []);
+		_paq.push(["trackPageView"]);
+		_paq.push(["enableLinkTracking"]);
+		_paq.push(["disableCookies"]);
 
-    const d = document;
-    const g = d.createElement('script');
-    const s = d.getElementsByTagName('script')[0];
-    g.async = true;
-    g.src = '/matomo.js'; // Load script from the local directory
-    s.parentNode.insertBefore(g, s);
-  }, []);
+		const u = "https://delfinowebsite.matomo.cloud/";
+		_paq.push(["setTrackerUrl", u + "matomo.php"]);
+		_paq.push(["setSiteId", "2"]);
 
-  return null;
+		const d = document;
+		const g = d.createElement("script");
+		const s = d.getElementsByTagName("script")[0];
+		g.async = true;
+		g.src = "/matomo.js"; // Load script from the local directory
+		s.parentNode.insertBefore(g, s);
+	}, []);
+
+	return null;
 };
 
 export default Matomo;
